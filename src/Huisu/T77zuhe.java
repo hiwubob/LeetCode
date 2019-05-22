2f8cf02ac2ae74e946dac7ad889df26fda0ea246
hiwubob <wubob@163.com>
2019-05-22 21:10:43 +0800

    Default Changelist

--- src/Huisu/T77zuhe.java
+++ src/Huisu/T77zuhe.java
@@ -1,30 +1,30 @@
 package Huisu;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class T77zuhe {
+//	给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。/**/
 	public List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> list = new LinkedList<>();
-		List<Integer> li = new LinkedList<>();
-		huisu(list, li, n, k);
-		return list;
+		List<List<Integer>> result = new ArrayList<>();
+		if (n <= 0 || k <= 0) {
+			return result;
+		}
+		dfs(result, new ArrayList<Integer>(), 1, n, k);
+		return result;
 	}
 
-	private void huisu(List<List<Integer>> list, List<Integer> li, int n, int k) {
-		if (k == 0) {
-			list.add(li);
-			return;
-		}
-		if (n < k) {
-			return;
-		}
-
-		for (int i = n; i >= 1; i--) {
-			List<Integer> copy = new LinkedList<>();
-			copy.addAll(li);
-			copy.add(i);
-			huisu(list, copy, i - 1, k - 1);
+	private void dfs(List<List<Integer>> result, ArrayList<Integer> list, int index, int n, int k) {
+		for (int i = index; i <= n; i++) {
+			list.add(i);
+			if (list.size() == k) {
+				result.add(new ArrayList<>(list));
+				list.remove(list.size() - 1);
+				continue;
+			}
+			dfs(result, list, i + 1, n, k);
+			list.remove(list.size() - 1);
 		}
 	}
 }
