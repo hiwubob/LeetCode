9f85089db597fb9e6d469e34365850e4a67cdf1c
hiwubob <wubob@163.com>
2019-05-24 14:27:04 +0800

    Default Changelist

--- src/StackandQueue/sortStackByStack.java
+++ src/StackandQueue/sortStackByStack.java
@@ -3,6 +3,9 @@ package StackandQueue;
 import java.util.Stack;
 
 public class sortStackByStack {
+    //一个栈中的元素为整形，
+    // 现在将该栈从顶到底按从大到小的顺序排列，
+    // 只允许申请一个栈。可以申请新的变量，但不能额外申请额外的数据结构
     public static void sortStackByStack(Stack<Integer> stack) {
         Stack<Integer> help = new Stack<>();
         while (!stack.isEmpty()) {
