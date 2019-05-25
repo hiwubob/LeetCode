64134e5e54170e3b695d8fe165f189603a0c6637
hiwubob <wubob@163.com>
2019-05-25 11:51:15 +0800

    Default Changelist

---
+++ src/StackandQueue/getNearLessNoRepeat.java
@@ -0,0 +1,26 @@
+package StackandQueue;
+
+import java.util.Stack;
+
+public class getNearLessNoRepeat {
+    public int[][] getNearLessNoRepeat(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                int popIndex = stack.pop();
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+                res[popIndex][0] = leftLessIndex;
+                res[popIndex][1] = i;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int popIndex = stack.pop();
+            int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+            res[popIndex][0] = leftLessIndex;
+            res[popIndex][1] = -1;
+        }
+        return res;
+    }
+}
