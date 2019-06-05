e9bfe0c5a6576f23428da90f03201e7e7e6d601e
hiwubob <wubob@163.com>
2019-06-05 20:43:36 +0800

    Default Changelist

--- src/Sort/insertSort.java
+++ src/Sort/insertSort.java
@@ -3,6 +3,16 @@ package Sort;
 public class insertSort {
     public void insertSort(int[] a) {
         for (int i = 1; i < a.length; i++) {
+            for (int j = i - 1; j >= 0 && a[j] > a[j + 1]; j--) {
+                a[j + 1] = a[j + 1] + a[j];
+                a[j] = a[j + 1] - a[j];
+                a[j + 1] = a[j + 1] - a[j];
+            }
+        }
+    }
+
+    public void insertSort2(int[] a) {
+        for (int i = 1; i < a.length; i++) {
             int tmp = a[i];
             int j;
             for (j = i - 1; j >= 0 && a[j] > tmp; j--) {
@@ -12,20 +22,12 @@ public class insertSort {
         }
     }
 
-    public void insert2(int[] a) {
-        for (int i = 1; i < a.length; i++) {
-            for (int j = i - 1; j >= 0 && a[j] > a[j + 1]; j--) {
-                a[j + 1] = a[j + 1] + a[j];
-                a[j] = a[j + 1] - a[j];
-                a[j + 1] = a[j + 1] - a[j];
-            }
-        }
-    }
+
 
     public static void main(String[] args) {
         int[] a = {6, 4, 7, 3, 8, 3};
         insertSort s = new insertSort();
-        s.insert2(a);
+        s.insertSort(a);
         for (int b : a) {
             System.out.print(b);
         }
