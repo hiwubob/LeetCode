19dff540c69ed07918f2717453fce94aee90689c
hiwubob <wubob@163.com>
2019-07-01 21:20:30 +0800

    0

---
+++ src/Dp/No221zuidazhengfangxing.java
@@ -0,0 +1,29 @@
+package Dp;
+
+public class No221zuidazhengfangxing {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        //init
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        //general
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
