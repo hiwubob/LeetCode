10edaaf725b3fefc5f94f91b225edc5d198aeaa6
hiwubob <wubob@163.com>
2019-07-02 19:51:25 +0800

    0

---
+++ src/greedy/No605zhonghua.java
@@ -0,0 +1,21 @@
+package greedy;
+
+public class No605zhonghua {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len=flowerbed.length;
+        int count=0;
+        for (int i = 0; i < len; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            int pre = i == 0 ? 0 : flowerbed[i - 1];
+            int next = i == len - 1 ? 0 : flowerbed[i + 1];
+            if (pre == 0 || next == 0) {
+                flowerbed[i] = 1;
+                count++;
+            }
+
+        }
+        return count>=n;
+    }
+}
