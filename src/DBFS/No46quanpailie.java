d008b300538eb8cf2a1e857f2114a941fa6e97f8
hiwubob <wubob@163.com>
2019-07-02 17:08:08 +0800

    0

---
+++ src/DBFS/No46quanpailie.java
@@ -0,0 +1,31 @@
+package DBFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No46quanpailie {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        dfs(res,new ArrayList<Integer>(),nums);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, ArrayList<Integer> leverList, int[] nums) {
+        if (leverList.size() == nums.length) {
+            res.add(new ArrayList<>(leverList));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leverList.contains(nums[i])) {
+                continue;
+            }
+            leverList.add(nums[i]);
+            dfs(res, leverList, nums);
+            leverList.remove(leverList.size() - 1);
+        }
+    }
+
+}
--- src/DBFS/T130solve.java
+++ src/DBFS/T130solve.java
@@ -2,14 +2,14 @@ package DBFS;
 
 public class T130solve {
     private int[] dx = {1, -1, 0, 0};
-    private int[] dy = {0, 0, 1 - 1};
+    private int[] dy = {0, 0, 1, -1};
     int row, col;
 
-    //扫边界
     public void solve(char[][] board) {
         if (board == null || board.length == 0) {
             return;
         }
+
         row = board.length;
         col = board[0].length;
         for (int i = 0; i < row; i++) {
@@ -19,26 +19,27 @@ public class T130solve {
                 }
             }
         }
+
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (board[i][j] == '0') {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
-
                 }
                 if (board[i][j] == 'M') {
-                    board[i][j] = '0';
+                    board[i][j] = 'O';
                 }
             }
         }
     }
 
     private void dfs130(char[][] board, int x, int y) {
-        if (x < 0 || x >= row || y < 0 || y >= col || board[x][y] != '0') {
+        if (x < 0 || x >= row || y < 0 || y >= col || board[x][y] != 'O') {
             return;
         }
         board[x][y] = 'M';
         for (int i = 0; i < dx.length; i++) {
             dfs130(board, x + dx[i], y + dy[i]);
         }
+
     }
 }
