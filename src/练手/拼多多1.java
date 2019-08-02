5f98742e0bae76c6eff436c2736e6e99f0243445
wubob@163.com <wubob@163.com>
2019-08-02 11:27:35 +0800

    Default Changelist

---
+++ src/练手/拼多多1.java
@@ -0,0 +1,14 @@
+package ¡∑ ÷;
+
+import java.util.Scanner;
+
+public class ∆¥∂‡∂‡1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i]=sc.nextInt();
+        }
+    }
+}
